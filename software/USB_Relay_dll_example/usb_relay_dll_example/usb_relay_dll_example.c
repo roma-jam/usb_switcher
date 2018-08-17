1aa2ea710fbf183c919211d1bb76f2c303144d2e
Roma Jam <jam_roma@yahoo.com>
2018-08-17 18:58:39 +0300

    + Made an alignment

--- software/USB_Relay_dll_example/usb_relay_dll_example/usb_relay_dll_example.c
+++ software/USB_Relay_dll_example/usb_relay_dll_example/usb_relay_dll_example.c
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
 	/* initialize instance */
 	HINSTANCE	usb_relay_dll;
 
-	printf("Import usb_relay.dll ...");
+	printf("Import usb_relay.dll ...................... ");
 
 	if (NULL == (usb_relay_dll = LoadLibrary("usb_relay.dll")))
 	{
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
 	}
 	printf("OK\n");
 
-	printf("Extract \"usb_relay_open\" function ... ");
+	printf("Extract \"usb_relay_open\" function ......... ");
 	relay_open = (p_usb_relay_open)GetProcAddress(usb_relay_dll, "usb_relay_open");
 	if (relay_open == NULL)
 	{
@@ -51,7 +51,7 @@ int main(int argc, char** argv)
 	}
 	printf("OK\n");
 
-	printf("Extract \"usb_relay_close\" function ... ");
+	printf("Extract \"usb_relay_close\" function ........ ");
 	relay_close = (p_usb_relay_close)GetProcAddress(usb_relay_dll, "usb_relay_close");
 	if (relay_close == NULL)
 	{
@@ -75,7 +75,7 @@ int main(int argc, char** argv)
 	}
 	printf("OK\n");
 
-	printf("Extract \"usb_relay_set_state\" function ... ");
+	printf("Extract \"usb_relay_set_state\" function .... ");
 	relay_set_state = (p_usb_relay_set_state)GetProcAddress(usb_relay_dll, "usb_relay_set_state");
 	if (relay_set_state == NULL)
 	{
@@ -90,7 +90,7 @@ int main(int argc, char** argv)
 	/* DEVICE COMMUNICATION */
 
 	/* open device */
-	printf("Device open ... ");
+	printf("Device open ............................... ");
 	void* device = relay_open(0x24DC, 0x0FFE, 0x0002, 0xFA1D);
 	if (device == NULL)
 	{
@@ -107,7 +107,7 @@ int main(int argc, char** argv)
 	Sleep(5000);
 
 	/* set device ON */
-	printf("Device set state ON ... ");
+	printf("Device set state ON ....................... ");
 	relay_set_state(device, USB_RELAY_DEVICE_STATE_ON, 1000);
 	printf("OK\n");
 
@@ -116,19 +116,17 @@ int main(int argc, char** argv)
 	Sleep(5000);
 
 	/* set device OFF */
-	printf("Device set state OFF ... ");
+	printf("Device set state OFF ...................... ");
 	relay_set_state(device, USB_RELAY_DEVICE_STATE_OFF, 1000);
 	printf("OK\n");
 
 	/* close device */
-	printf("Device close ... ");
+	printf("Device close .............................. ");
 	relay_close(device);
 	printf("OK\n");
 	/* ******************** */
 
 	FreeLibrary(usb_relay_dll);
-
-	get_key();
     return 0;
 }
 
